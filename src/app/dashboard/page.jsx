48c88f986c67f336f877e02be3bb3553942f3c57
Mark Skews <mark.skews@gmail.com>
2025-05-19 11:41:21 +0300

    feat: rename DashboardPage to Dashboard and update heading for consistency

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function DashboardPage() {
+export default function Dashboard() {
   return (
     <div className='p-10'>
-      <h1 className='text-2xl font-bold mb-5'>Dashboard Page</h1>
+      <h1 className='text-2xl font-bold mb-5'>Dashboard</h1>
     </div>
   );
 }
---
+++ src/context/video.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+  ChangeEvent,
+} from 'react';
+
+const initialState = {
+  script: 'Script...',
+  images: [] as string[],
+  audio: '',
+  captions: [] as object[],
+  loading: false,
+};
+
+interface VideoContextType {
+  script: string;
+  setScript: Dispatch<SetStateAction<string>>;
+  images: string[];
+  setImages: Dispatch<SetStateAction<string[]>>;
+  audio: string;
+  setAudio: Dispatch<SetStateAction<string>>;
+  captions: object[];
+  setCaptions: Dispatch<SetStateAction<object[]>>;
+  loading: boolean;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+const VideoContext = createContext<VideoContextType | undefined>(undefined);
+
+export const VideoProvider = ({ children }: { children: ReactNode }) => {
+  const [script, setScript] = useState(initialState.script);
+  const [images, setImages] = useState(initialState.images);
+  const [audio, setAudio] = useState(initialState.audio);
+  const [captions, setCaptions] = useState(initialState.captions);
+  const [loading, setLoading] = useState(initialState.loading);
+
+  return (
+    <VideoContext.Provider
+      value={{
+        script,
+        setScript,
+        images,
+        setImages,
+        audio,
+        setAudio,
+        captions,
+        setCaptions,
+        loading,
+        setLoading,
+      }}
+    >
+      {children}
+    </VideoContext.Provider>
+  );
+};
